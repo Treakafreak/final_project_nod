8d8a6562030f59a1475c285caa7ca89cd9e7e86e
Treakafreak <105550860+Treakafreak@users.noreply.github.com>
2022-06-13 18:51:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ with st.echo(code_location='below'):
             x.append(requests.get(i).json()['features'])
         return x
     data=get_data()
+    def find_min(df, coor):
+        x=((df['lat']-coor[0])**2+(df['lon']-coor[1])**2).argmin()
+        return x
     col1, col2=st.columns(2)
     """
     Для начала посмотрим на статистику по районам города
@@ -43,7 +46,7 @@ with st.echo(code_location='below'):
     districts.explore('count', m=m)
     folium_static(m)
     col1, col2=st.columns(2)
-        """
+    """
     Теперь найдем ближайшую к вам больницу оказывающую такой типо помощи. Введите адресс из города который вы выбрали выше
     """
     with col1:
@@ -58,25 +61,26 @@ with st.echo(code_location='below'):
         r.json()
         if len(r.json()['features']) >0:
             coordinates=r.json()['features'][0]['geometry']['coordinates']
-            st.write(coordinates)
         else:
-            st.write('Попробуйте по другому')
+            st.write('Попробуйте написать по другому')
     hospitals=[]
     for i in hos:
         hospitals.append([i['properties']['name'],i['properties'][type_help],i['geometry']['coordinates'] ])
     hospitals=pd.DataFrame(hospitals)
     hospitals= hospitals[[0,1]].join(pd.DataFrame(hospitals[2].tolist(), columns=['lat','lon']))
     hospitals= hospitals[hospitals[1]==1]
-    def find_min(df, coor):
-        x=((df['lat']-coor[0])**2+(df['lon']-coor[1])**2).argmin()
-        return x
-    if len(coordinates)>0:
-        x=find_min(hospitals,coordinates)
-        st.write(x)
-        a=hospitals.iloc[x]
-        url='https://nominatim.openstreetmap.org/reverse?'
-        params={'format':'json','lon': a['lat'], 'lat':a['lon']}
-        r=requests.get(url,params)
-        name_min=r.json()['display_name']
-        st.write('Ближайшая больница предоставляющуя помощь вида '+type_help+' это '+a[0]+' находящаяся по адресу '+ name_min)
+    if len(hospitals)==0:
+        st.write('В вашем городе нет таких МО')
+    else:
+        if len(coordinates)>0:
+            x=find_min(hospitals,coordinates)
+            a=hospitals.iloc[x]
+            url='https://nominatim.openstreetmap.org/reverse?'
+            params={'format':'json','lon': a['lat'], 'lat':a['lon']}
+            r=requests.get(url,params)
+            name_min=r.json()['display_name']
+            st.write('Ближайшая больница предоставляющуя помощь вида '+type_help+' это '+a[0]+' находящаяся по адресу '+ name_min)
+         else:
+            st.write('Попробуйте по другому')
+       
         
